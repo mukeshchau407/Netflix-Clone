b0717a9a9bcce9466a101de1eb0270fc60f1c4ae
mukeshchau407 <chymukesh5@gmail.com>
2025-05-09 12:04:19 +0545

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "./Navbar.css";
-import logo from "../../assets/logo.png";
+import logo1 from "../../assets/logo1.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
@@ -8,32 +8,44 @@ import caret_icon from "../../assets/caret_icon.svg";
 import { logout } from "../../firebase-config";
 
 const Navbar = () => {
-
   const navRef = useRef();
-  
-  useEffect(()=>{
-    window.addEventListener('scroll', ()=>{
-      if(window.scrollY >= 80){
-        navRef.current.classList.add('nav-dark')
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add("nav-dark");
+      } else {
+        navRef.current.classList.remove("nav-dark");
       }
-      else{
-        navRef.current.classList.remove('nav-dark')
-      }
-    })
-  })
+    });
+  });
 
   return (
     <div ref={navRef} className="navbar">
       <div className="navbar-left">
-        <img src={logo} alt="" />
+        <img src={logo1} alt="" />
 
         <ul>
-          <li ><a href="#" active>Home</a></li>
-          <li><a href="">TV Shows</a></li>
-          <li><a href="">Movies</a></li>
-          <li><a href="">News & Popular</a></li>
-          <li><a href="">My List</a></li>
-          <li><a href="">Browse by Languages</a></li>
+          <li>
+            <a href="#" active>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="">TV Shows</a>
+          </li>
+          <li>
+            <a href="">Movies</a>
+          </li>
+          <li>
+            <a href="">News & Popular</a>
+          </li>
+          <li>
+            <a href="">My List</a>
+          </li>
+          <li>
+            <a href="">Browse by Languages</a>
+          </li>
         </ul>
       </div>
 
@@ -49,7 +61,13 @@ const Navbar = () => {
           <img src={profile_img} alt="" className="profile" />
           <img src={caret_icon} alt="" />
           <div className="dropdown">
-            <p onClick={()=>{logout()}}>Sign out of Netflix</p>
+            <p
+              onClick={() => {
+                logout();
+              }}
+            >
+              Sign out of Netflix
+            </p>
           </div>
         </div>
       </div>
